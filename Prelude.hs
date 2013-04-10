ec17548065d0046f072ec72f76e71a7e323250cf
Németh Boldizsár <lazac-@hotmail.com>
2013-04-11 01:13:32 +0200

    first commit, basic classes and booleans

---
+++ Prelude.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE NoImplicitPrelude, TypeFamilies #-}
+
+module Classy.Prelude where
+
+import Prelude (id, const, flip, (.), String) 
+import Data.Maybe (Maybe(..), fromJust)
+import Classy.Bool
+
+-- | Something that can be displayed as text
+class Show a where
+  show :: a -> String
+
+-- | Something that can be read from a console or textfile
+class Read a where
+  read :: String -> a
+  
+-- | Something that can act as a function.
+-- Minimal complete definition: apply or partial
+class Function f where
+  type FunArg f :: *
+  type FunRes f :: *
+  apply :: f -> (FunArg f -> FunRes f)
+  apply f = fromJust . partial f
+  partial :: f -> (FunArg f -> Maybe (FunRes f))
+  partial f = Just . apply f
+
+instance Function (a -> b) where
+  type FunArg (a -> b) = a
+  type FunRes (a -> b) = b
+  apply = id
+
+-- | Something that has a zero element and addition
+-- Monoid laws:
+--   empty ++ b = b
+--   b ++ empty = b                            (zero laws)
+--   (a ++ b) ++ c = a ++ (b ++ c)             (associativity)
+--   (a ++ b) >>= f = (a >>= f) ++ (b >>= f)   (left distribution)
+class Monoid m where
+  empty :: m
+  (++) :: m -> m -> m
+  
+-- | Something that has a structure that can be mapped.
+-- Functor laws:
+--   map id = id
+--   map (f.g) = map f . map g
+class Functor f where
+  map :: (a -> b) -> f a -> f b
+  
+infixl 4 <@>  
+
+class (Functor p) => Applicative p where
+  return   :: a -> p a               -- value lifting
+  (<@>)  :: p (a -> b) -> p a -> p b -- lifted application
+  
+  (<$>)  :: (a -> b) -> p a -> p b   -- function lifting
+  pa <$> pb = return pa <@> pb
+  
+  (>>)   :: p a -> p b -> p b        -- when the result is independent of the first
+  pa >> pb = const id <$> pa <@> pb
+  (<<)   :: p b -> p a -> p b        -- when the result is independent of the second  
+  pa << pb = flip (const id) <$> pa <@> pb
+
+  
+class (Applicative m) => Monad m where
+  -- | Minimal complete definition: one of join or (>>=).
+  (>>=) :: m a -> (a -> m b) -> m b -- bind
+  join  :: m (m a) -> m a           -- combining levels of structure
+ 
+  ma >>= k  = join (map k ma)
+  join mma  = mma >>= id  
+  
+-- | MonadZero laws:
+--  zero >>= f = zero
+class (Monad mz) => MonadZero mz where
+  zero :: mz a  
+
+-- | MonadPlus should satisfy monoid laws with zero as empty
+-- and <+> as ++
+class (MonadZero mp) => MonadPlus mp where
+  (<+>) :: mp a -> mp a -> mp a
+  
+-- | MonadPlus should satisfy monoid laws with zero as empty
+-- and orElse as ++
+class (MonadZero mo) => MonadOr mo where
+  orElse :: mo a -> mo a -> mo a
+ 
+class (Monad m) => MonadFail m where
+  fail :: (Show s) => s -> m a
